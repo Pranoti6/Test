615c153cf37908a248ecadde57c240575c6e5e5c
Pranoti Pawar <Pranoti Pawar@LAPTOP-1CIBB2A9>
2022-09-24 12:58:17 +0530

    added

--- maven001/src/test/java/Setup/Base.java
+++ maven001/src/test/java/Setup/Base.java
@@ -3,7 +3,7 @@ package Setup;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Base {
 	
