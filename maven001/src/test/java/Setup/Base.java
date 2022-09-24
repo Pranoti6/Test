9a57e6f68b0a985174c785058bd6f7b8f36018cd
Pranoti Pawar <Pranoti Pawar@LAPTOP-1CIBB2A9>
2022-09-24 12:48:55 +0530

    added new method

--- maven001/src/test/java/Setup/Base.java
+++ maven001/src/test/java/Setup/Base.java
@@ -3,6 +3,7 @@ package Setup;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
 	
@@ -18,7 +19,11 @@ public class Base {
 		return driver;
 	}
 	
-	
+	public static WebDriver openTheFireFoxBrowser() {
+		System.setProperty("webdriver.fireFox.driver", "C:\\selnium\\edgedriver_win64\\msedgedriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		return driver;
+	}
 	
 	
 
